0ac9d85d7fd4e56fdf6ea211f737ff39253e2402
Facundo <faqqu.uri@gmail.com>
2020-09-14 12:55:39 -0300

    configurando

--- client/App.js
+++ client/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from "react-native-paper";
 import App from "./src";
 import { theme } from "./src/core/theme";
-import store from "./src/store";
+import store from "./src/redux/store";
 import { Provider as StoreProvider } from "react-redux";
 
 export default const App = () => (
